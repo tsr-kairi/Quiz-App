cdf71e323632896455c82955fb7c477f9e316386
Ranjit Kairi <ranjitkoiri009@gmail.com>
2021-07-27 12:34:46 +0530

    quiz-app

---
+++ src/Helpers/QuestionBank.js
@@ -0,0 +1,37 @@
+export const Questions = [
+  {
+    prompt: "What is 1+3?",
+    optionA: "24",
+    optionB: "21",
+    optionC: "4",
+    optionD: "2",
+    answer: "optionC",
+  },
+
+  {
+    prompt: "Which is a Streaming platform?",
+    optionA: "Twitter",
+    optionB: "Twitch",
+    optionC: "Google",
+    optionD: "Robinhood",
+    answer: "B",
+  },
+
+  {
+    prompt: "What is the value of 7?",
+    optionA: "4",
+    optionB: "19",
+    optionC: "2",
+    optionD: "22",
+    answer: "C",
+  },
+
+  {
+    prompt: "Best teach youtube channel?",
+    optionA: "PedroTeach",
+    optionB: "Dev ED",
+    optionC: "Friendship",
+    optionD: "TeachWith-TSR",
+    answer: "D",
+  },
+];
